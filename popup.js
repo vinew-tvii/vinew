1fa167b7b0675ca5fea00b3888cda693b6e1171f
6100m (pronounced Gloom) <77129415+6100m@users.noreply.github.com>
2023-02-15 21:26:48 -0700

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,18 @@
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function openForm() {
+  alert("UNINTENDED FOR PROUDCTION ENVIROMENTS. REMOVE ON VINEWS RELEASE. -6100m")
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+const data = "Nintendo TVii allows you to search around your favorite shows and movies, share live moments using tvtag"
+
+// All from w3schools middleware and jan luna, 6100m takes no credit for this
